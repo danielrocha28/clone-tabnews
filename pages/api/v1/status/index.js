1f5c2e30c415c9e976dc89b322f17201eeb6aaff
danielrocha28 <danielrocha.2805@gmail.com>
2025-06-05 21:26:59 -0300

    feat/finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
-async function status (request, response){
-    const result = await database.query("SELECT 1 + 1 as sum;")
-    console.log(result.rows);
-    response.status(200).json({ chave: "são acima da media" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const { rows: versionRows } = await database.query("SHOW server_version;");
+  const { rows: maxConnectionsRows } = await database.query(
+    "SHOW max_connections;",
+  );
+  const { rows: openedConnectionsRows } = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const version = versionRows[0].server_version;
+  const maxConnections = parseInt(maxConnectionsRows[0].max_connections);
+  const openedConnections = openedConnectionsRows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      version: version,
+      max_connections: maxConnections,
+      opened_connections: openedConnections,
+    },
+  });
 }
 
-export default status;  
+export default status;
