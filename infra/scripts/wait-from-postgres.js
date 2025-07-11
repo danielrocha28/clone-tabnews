e0f98eba7cb5a82d2c9c2c9e16221518998b8e70
danielrocha28 <danielrocha.2805@gmail.com>
2025-07-11 18:47:10 -0300

    feat-change-layout

--- infra/scripts/wait-from-postgres.js
+++ infra/scripts/wait-from-postgres.js
@@ -1,18 +1,22 @@
 const { exec } = require("node:child_process");
 
+let spinner = ["/", "/", "/", "/", "-", "-", "-", "-", "\\","\\","\\","\\","|","|","|","|", ];
+let i = 0;
+let awaintMessage = "🔴 Awaiting for postgres";
+
 function checkPostgres() {
-  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+  exec("docker exec postgres-dev pg_isready --host localhost ", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-    process.stdout.write('.')
+      process.stdout.cursorTo(awaintMessage.length + 1);
+      process.stdout.write(`${spinner[i++ % spinner.length]}`);
       checkPostgres();
       return;
     }
-    console.log("\n🟢 postgres esta pronto e aceitando conexoes\n");
+
+    console.log("\n🟢 Postgres is ready!");
   }
 }
-
-process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões ");
-
+process.stdout.write(awaintMessage);
 checkPostgres();
