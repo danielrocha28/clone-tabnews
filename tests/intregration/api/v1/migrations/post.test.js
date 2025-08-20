42ad7a00dd94d20a1bd6094695a9622f6f2c836b
danielrocha28 <danielrocha.2805@gmail.com>
2025-08-19 21:01:45 -0300

    test: rename all descriptions to use new pattern

--- tests/intregration/api/v1/migrations/post.test.js
+++ tests/intregration/api/v1/migrations/post.test.js
@@ -6,26 +6,38 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
+
+        const respondeBody1 = await response1.json();
+
+        expect(Array.isArray(respondeBody1)).toBe(true);
+        expect(respondeBody1.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const respondeBody2 = await response2.json();
+
+        expect(Array.isArray(respondeBody2)).toBe(true);
+        expect(respondeBody2.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const respondeBody1 = await response1.json();
-
-  expect(Array.isArray(respondeBody1)).toBe(true);
-
-  expect(respondeBody1.length).toBeGreaterThan(0);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const respondeBody2 = await response2.json();
-
-  expect(Array.isArray(respondeBody2)).toBe(true);
-
-  expect(respondeBody2.length).toBe(0);
 });
