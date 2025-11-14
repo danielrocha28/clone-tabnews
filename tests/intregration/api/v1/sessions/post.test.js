e30c1bb51cdc3b399d17a7620548d20844121273
danielrocha28 <danielrocha.2805@gmail.com>
2025-11-14 20:13:54 -0300

    feat: implement session creation

---
+++ tests/intregration/api/v1/sessions/post.test.js
@@ -0,0 +1,149 @@
+import orchestrator from "tests/orchestrator.js";
+import setCookieParser from "set-cookie-parser";
+import { version as uuidVersion } from "uuid";
+import session from "models/sessions";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.cleanDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/sessions", () => {
+  describe("Anonymous user", () => {
+    test("With incorrect `email` but correct `password`", async () => {
+      await orchestrator.createUser({
+        password: "senha-correta",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "emailerrado@curso.dev",
+          password: "senha-correta",
+        }),
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Dados de autentificação não conferem.",
+        action: "Verifique se os dados informados estão corretos.",
+        status_code: 401,
+      });
+    });
+    test("With incorrect `password` but correct `email`", async () => {
+      await orchestrator.createUser({
+        email: "email.correto@curso.dev",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "email.correto@curso.dev",
+          password: "senha-errada",
+        }),
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Dados de autentificação não conferem.",
+        action: "Verifique se os dados informados estão corretos.",
+        status_code: 401,
+      });
+    });
+    test("With incorrect `password` and incorrect `email`", async () => {
+      await orchestrator.createUser();
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "email.incorreto@curso.dev",
+          password: "senha-incorreta",
+        }),
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Dados de autentificação não conferem.",
+        action: "Verifique se os dados informados estão corretos.",
+        status_code: 401,
+      });
+    });
+
+    test("With correct `email` and correct `password`", async () => {
+      const createdUser = await orchestrator.createUser({
+        email: "tudo.correto@curso.dev",
+        password: "tudo-correta",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "tudo.correto@curso.dev",
+          password: "tudo-correta",
+        }),
+      });
+
+      expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        token: responseBody.token,
+        user_id: createdUser.id,
+        expires_at: responseBody.expires_at,
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.expires_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const expires_at = new Date(responseBody.expires_at);
+
+      const created_at = new Date(responseBody.created_at);
+
+      const diff = Math.abs(
+        expires_at - created_at - session.EXPIRATION_IN_MILLISECONDS,
+      );
+      expect(diff).toBeLessThan(5000);
+
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      });
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: responseBody.token,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+        path: "/",
+        httpOnly: true,
+      });
+    });
+  });
+});
